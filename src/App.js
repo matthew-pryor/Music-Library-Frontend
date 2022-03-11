eb00c418849e0f87ba117ace6289d12457d792d8
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-11 14:18:14 -0600

    created component DisplaySongs jsx and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+
 
 function App() {
+
+  const [entries, setEntries] = useState([{title: "Jack Sparrow", artist: "Lonely island", album: "Turleneck & Chain", release_date: "2017-01-01", genre: "comedy"}])
+
+  function addNewSong(entry){
+    
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1>MUSIC LIBRARY WEBSITE HEADER GOES HERE</h1>
+
+      <h3>NAV BAR GOES HERE</h3>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Release Date</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {entries.map((entry) => {
+              return(
+                <tr>
+                  <td>{entry.title}</td>
+                  <td>{entry.artist}</td>
+                  <td>{entry.album}</td>
+                  <td>{entry.release_date}</td>
+                  <td>{entry.genre}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    
     </div>
   );
 }
