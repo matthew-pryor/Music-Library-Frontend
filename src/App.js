4cc2c62330a3ee9318536da7fc808a0a61df5149
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-11 14:28:34 -0600

    modified displaysongs component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import DisplaySongs from "./Components/DisplaySongs/DisplaySongs";
 
 
 
@@ -21,33 +22,7 @@ function App() {
 
       <h3>NAV BAR GOES HERE</h3>
 
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Artist</th>
-              <th>Album</th>
-              <th>Release Date</th>
-              <th>Genre</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {entries.map((entry) => {
-              return(
-                <tr>
-                  <td>{entry.title}</td>
-                  <td>{entry.artist}</td>
-                  <td>{entry.album}</td>
-                  <td>{entry.release_date}</td>
-                  <td>{entry.genre}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      <DisplaySongs parentEntries = {entries}/>
     
     </div>
   );
---
+++ src/Components/DisplaySongs/DisplaySongs.jsx
@@ -0,0 +1,35 @@
+
+
+const DisplaySongs = (props) => {
+    return ( 
+    <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Release Date</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {props.parentEntries.map((entry) => {
+              return(
+                <tr>
+                  <td>{entry.title}</td>
+                  <td>{entry.artist}</td>
+                  <td>{entry.album}</td>
+                  <td>{entry.release_date}</td>
+                  <td>{entry.genre}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+    </div>
+     );
+}
+ 
+export default DisplaySongs;
